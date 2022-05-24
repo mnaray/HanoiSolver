8cdc260cbfcced53f879af0c78afd6dfad6d5536
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-05-24 21:41:09 +0200

    removed objects and replace them with normal integers

--- HanoiSolver/Program.cs
+++ HanoiSolver/Program.cs
@@ -9,29 +9,42 @@ namespace HanoiSolver
         {
             // This will solve the towers of Hanoi.
 
+            Console.WriteLine("Enter the amount of plates you want to use for the game.");
+            int plateAmount = Convert.ToInt32(Console.ReadLine());
             Stack towerLeft = new Stack();
             Stack towerMiddle = new Stack();
             Stack towerRight = new Stack();
 
-            Plate plateSmall = new Plate();
-            plateSmall.plateSize = 1;
-            Plate plateMedium = new Plate();
-            plateSmall.plateSize = 2;
-            Plate plateBig = new Plate();
-            plateSmall.plateSize = 3;
+            for (int i = 0; i < plateAmount; i++)
+            {
+                towerLeft.Push(i + 1);
+            }
 
 
+
+
+
+
+
+            Console.WriteLine("---------");
+            foreach (var item in towerLeft)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------");
+            foreach (var item in towerMiddle)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------");
+            foreach (var item in towerRight)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------");
 
 
             Console.ReadKey();
-        }
-    }
-
-    class Plate
-    {
-        public int plateSize {
-            get;
-            set;
         }
     }
 }
