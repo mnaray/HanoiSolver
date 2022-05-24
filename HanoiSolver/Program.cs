15102df524e6cda47079ccd38dac00104603db70
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-05-24 20:50:45 +0200

    initial commit

---
+++ HanoiSolver/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HanoiSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This will solve the towers of Hanoi.
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
